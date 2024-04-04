901a88af3dca6513442db14f9ff63635548ad771
sergiomanr <114090605+sergiomanr@users.noreply.github.com>
2024-04-04 15:31:36 +0200

    .

---
+++ UPM/TECW/Practica5_blog/prac5/migrations/20240425-CrearTablaAttachments.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+    'Attachments',    {
+        id: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: Sequelize.INTEGER
+        },
+        createdAt: {
+            type: Sequelize.DATE
+        },
+        updatedAt: {
+            type: Sequelize.DATE
+        },
+        mime: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        url: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        image: {
+            type: Sequelize.BLOB('long'),
+            allowNull: true
+        }
+    },
+    {
+    sync: {force: true}
+    }
+    );
+    },
+    down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Attachments');
+    }
+};
---
+++ UPM/TECW/Practica5_blog/prac5/migrations/20240425-CrearTablaPost.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+    'Posts',    {
+        id: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: Sequelize.INTEGER
+        },
+        createdAt: {
+            type: Sequelize.DATE
+        },
+        updatedAt: {
+            type: Sequelize.DATE
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        body: {
+            type: Sequelize.TEXT,
+            allowNull: false
+        },
+        attachmentId: {
+            type: Sequelize.INTEGER,
+        } 
+    },
+    {
+    sync: {force: true}
+    }
+    );
+    },
+    down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Posts');
+    }
+};
